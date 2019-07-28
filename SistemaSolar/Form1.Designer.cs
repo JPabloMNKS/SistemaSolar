cc6aaef86512b8957c7ebec4768cbb52933dd1af
unknown <jpablo.bedoya7@gmail.com>
2019-07-27 21:49:58 -0400

    Añadiendo la base, de la primera fase

--- SistemaSolar/Form1.Designer.cs
+++ SistemaSolar/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(655, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SALIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 394);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(221, 394);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(184, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "textito";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SistemaSolar/Form1.cs
+++ SistemaSolar/Form1.cs
@@ -16,5 +16,94 @@ namespace SistemaSolar
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+
+            if(e.X >= 47 && e.X <= 131 && e.Y >= 183 && e.Y <= 256)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "SOL";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 153 && e.X <= 171 && e.Y >= 215 && e.Y <= 230)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "MERCURIO";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 193 && e.X <= 232 && e.Y >= 207 && e.Y <= 236)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "VENUS";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 256 && e.X <= 301 && e.Y >= 203 && e.Y <= 236)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "TIERRA";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 322 && e.X <= 361 && e.Y >= 207 && e.Y <= 235)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "MARTE";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 386 && e.X <= 473 && e.Y >= 188 && e.Y <= 255)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "JUPITER";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 510 && e.X <= 584 && e.Y >= 197 && e.Y <= 251)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "SATURNO";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 616 && e.X <= 668 && e.Y >= 201 && e.Y <= 241)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "URANO";
+                label1.Location = new Point(e.X + 25, e.Y + 25);
+            }
+            else if (e.X >= 693 && e.X <= 736 && e.Y >= 206 && e.Y <= 240)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "NEPTUNO";
+                label1.Location = new Point(e.X - 25, e.Y - 25);
+            }
+            else if (e.X >= 765 && e.X <= 785 && e.Y >= 210 && e.Y <= 230)
+            {
+                this.Cursor = Cursors.Hand;
+                label1.Visible = true;
+                label1.Text = "PLUTON";
+                label1.Location = new Point(e.X - 25, e.Y - 25);
+            }
+            else
+            {
+                this.Cursor = Cursors.Default;
+                label1.Visible = false;
+            }
+        }
+
+
     }
 }
